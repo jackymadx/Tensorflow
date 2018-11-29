0d261aed3ac708c78f2dc3b547adae5362e8d848
jackymadx <jacky.chan@madxstudio.com>
2018-11-29 12:22:15 +0800

    Update the file

--- 01_Introduction to Tensorflow/07_Working_with_Data_Sources/07_data_gathering.py
+++ 01_Introduction to Tensorflow/07_Working_with_Data_Sources/07_data_gathering.py
@@ -17,42 +17,44 @@ logging.basicConfig(format='%(filename)s - %(asctime)s - %(message)s', level=log
 from sklearn import datasets
 
 iris = datasets.load_iris()
-print(len(iris.data))
-print(len(iris.target))
-print(iris.data[0])
-print(set(iris.target))
+logging.info(len(iris.data))
+logging.info(len(iris.target))
+logging.info(iris.data[0])
+logging.info(set(iris.target))
 
 # Low Birthrate Data
 import requests
 
 birthdata_url = 'https://github.com/nfmcclure/tensorflow_cookbook/raw/master/01_Introduction/07_Working_with_Data_Sources/birthweight_data/birthweight.dat'
+logging.info(birthdata_url)
 birth_file = requests.get(birthdata_url)
 birth_data = birth_file.text.split('\r\n')
 birth_header = birth_data[0].split('\t')
 birth_data = [[float(x) for x in y.split('\t') if len(x)>=1] for y in birth_data[1:] if len(y)>=1]
-print(len(birth_data))
-print(len(birth_data[0]))
+logging.info(len(birth_data))
+logging.info(len(birth_data[0]))
 
 
 # Housing Price Data
 import requests
 
 housing_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data'
+logging.info(housing_url)
 housing_header = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
 housing_file = requests.get(housing_url)
 housing_data = [[float(x) for x in y.split(' ') if len(x)>=1] for y in housing_file.text.split('\n') if len(y)>=1]
-print(len(housing_data))
-print(len(housing_data[0]))
+logging.info(len(housing_data))
+logging.info(len(housing_data[0]))
 
 
 # MNIST Handwriting Data
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-print(len(mnist.train.images))
-print(len(mnist.test.images))
-print(len(mnist.validation.images))
-print(mnist.train.labels[1,:])
+logging.info(len(mnist.train.images))
+logging.info(len(mnist.test.images))
+logging.info(len(mnist.validation.images))
+logging.info(mnist.train.labels[1,:])
 
 # CIFAR-10 Image Category Dataset
 # The CIFAR-10 data ( https://www.cs.toronto.edu/~kriz/cifar.html ) contains 60,000 32x32 color images of 10 classes.
@@ -63,9 +65,9 @@ print(mnist.train.labels[1,:])
 # Running this command requires an internet connection and a few minutes to download all the images.
 (X_train, y_train), (X_test, y_test) = tf.contrib.keras.datasets.cifar10.load_data()
 
-print(X_train.shape)
-print(y_train.shape)
-print(y_train[0,])  # this is a frog
+logging.info(X_train.shape)
+logging.info(y_train.shape)
+logging.info(y_train[0,])  # this is a frog
 
 # Plot the 0-th image (a frog)
 from PIL import Image
@@ -80,6 +82,7 @@ from zipfile import ZipFile
 
 # Get/read zip file
 zip_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip'
+logging.info(zip_url)
 r = requests.get(zip_url)
 z = ZipFile(io.BytesIO(r.content))
 file = z.read('SMSSpamCollection')
@@ -89,9 +92,9 @@ text_data = text_data.encode('ascii',errors='ignore')
 text_data = text_data.decode().split('\n')
 text_data = [x.split('\t') for x in text_data if len(x)>=1]
 [text_data_target, text_data_train] = [list(x) for x in zip(*text_data)]
-print(len(text_data_train))
-print(set(text_data_target))
-print(text_data_train[1])
+logging.info(len(text_data_train))
+logging.info(set(text_data_target))
+logging.info(text_data_train[1])
 
 
 # Movie Review Data
@@ -100,6 +103,7 @@ import io
 import tarfile
 
 movie_data_url = 'http://www.cs.cornell.edu/people/pabo/movie-review-data/rt-polaritydata.tar.gz'
+logging.info(movie_data_url)
 r = requests.get(movie_data_url)
 # Stream data into temp object
 stream_data = io.BytesIO(r.content)
@@ -124,15 +128,16 @@ for line in neg:
     neg_data.append(line.decode('ISO-8859-1').encode('ascii',errors='ignore').decode())
 tar_file.close()
 
-print(len(pos_data))
-print(len(neg_data))
-print(neg_data[0])
+logging.info(len(pos_data))
+logging.info(len(neg_data))
+logging.info(neg_data[0])
 
 
 # The Works of Shakespeare Data
 import requests
 
 shakespeare_url = 'http://www.gutenberg.org/cache/epub/100/pg100.txt'
+logging.info(shakespeare_url)
 # Get Shakespeare text
 response = requests.get(shakespeare_url)
 shakespeare_file = response.content
@@ -140,7 +145,7 @@ shakespeare_file = response.content
 shakespeare_text = shakespeare_file.decode('utf-8')
 # Drop first few descriptive paragraphs.
 shakespeare_text = shakespeare_text[7675:]
-print(len(shakespeare_text))
+logging.info(len(shakespeare_text))
 
 
 # English-German Sentence Translation Data
@@ -148,6 +153,7 @@ import requests
 import io
 from zipfile import ZipFile
 sentence_url = 'http://www.manythings.org/anki/deu-eng.zip'
+logging.info(sentence_url)
 r = requests.get(sentence_url)
 z = ZipFile(io.BytesIO(r.content))
 file = z.read('deu.txt')
@@ -157,6 +163,6 @@ eng_ger_data = eng_ger_data.encode('ascii',errors='ignore')
 eng_ger_data = eng_ger_data.decode().split('\n')
 eng_ger_data = [x.split('\t') for x in eng_ger_data if len(x)>=1]
 [english_sentence, german_sentence] = [list(x) for x in zip(*eng_ger_data)]
-print(len(english_sentence))
-print(len(german_sentence))
-print(eng_ger_data[10])
+logging.info(len(english_sentence))
+logging.info(len(german_sentence))
+logging.info(eng_ger_data[10])
